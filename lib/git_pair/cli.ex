36a21648845533657000925ee76446811333e184
Amadeu Cavalcante <amadeu.filho@pipefy.com>
2020-03-14 10:19:28 -0300

    :sparkles: Introduce parse, execute, and add command
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>

--- lib/git_pair/cli.ex
+++ lib/git_pair/cli.ex
@@ -3,8 +3,31 @@ defmodule GitPair.CLI do
   GitPair.CLI is the entrypoint for the git-pair utility.
   """
 
+  alias GitPair.Actions
+
+  @switches [
+    add: :string,
+    help: :boolean,
+    h: :boolean
+  ]
+
+  @aliases [
+    a: :add,
+    h: :help
+  ]
+
   def main(argv) do
-    IO.inspect(argv)
+    argv
+    |> parse_args
+    |> parse_command
+    |> execute_command
+  end
+
+  defp parse_args(args), do: OptionParser.parse(args, switches: @switches, aliases: @aliases)
+
+  defp parse_command({_, [action | args], _}), do: {action, args}
+
+  defp execute_command({action, args}) do
+    apply(Actions, String.to_atom(action), [args])
   end
 end
-
