7ed15a8d7774957ed17631279aaf5f3c8a348029
Amadeu Cavalcante <amadeu.filho@pipefy.com>
2020-03-14 10:35:22 -0300

    :sparkles: Execute pair add command
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>

--- lib/git_pair/actions.ex
+++ lib/git_pair/actions.ex
@@ -1,5 +1,8 @@
 defmodule GitPair.Actions do
+  @git_config ["config", "--add", "pair.coauthor"]
+
   def add([username]) do
+    {_, 0} = System.cmd("git", @git_config ++ [username])
     {:ok, "Added user #{username}"}
   end
 
