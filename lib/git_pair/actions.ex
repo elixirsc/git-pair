4a10abc8bd01941b68567581c251bcf77f4fca26
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2020-05-14 20:42:29 -0300

    Implement status command
    Co-authored-by: amadeu01 <amadeu01@users.noreply.github.com>
    Co-authored-by: lcidral <lcidral@users.noreply.github.com>
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>

--- lib/git_pair/actions.ex
+++ lib/git_pair/actions.ex
@@ -2,6 +2,8 @@ defmodule GitPair.Actions do
   @git_config "config"
   @key "pair.coauthor"
 
+  @success_exit_status 0
+
   def add(username) do
     command("--add", username)
 
@@ -10,15 +12,40 @@ defmodule GitPair.Actions do
 
   def rm(username) do
     command("--unset", username)
+    |> output("User #{username} removed")
+  end
 
-    {:ok, "User #{username} removed"}
+  def status() do
+    command("--get-all")
+    |> output("Pairing with: ")
+  end
+
+  defp output({"", @success_exit_status}, message) do
+    {:ok, message}
+  end
+
+  defp output({result, @success_exit_status}, message) do
+    # TODO: Add support to message templates
+    {:ok, "#{message}\n\n#{result}"}
+  end
+
+  defp output({"", _failed_exit_status}, _message) do
+    {:error, "Failed to execute command"}
+  end
+
+  defp output({result, _failed_exit_status}, _message) do
+    {:error, result}
+  end
+
+  defp command(action) do
+    System.cmd("git", [@git_config, action, @key])
   end
 
   defp command(action, [username]) do
-    {_, 0} = System.cmd("git", [@git_config, action, @key, username])
+    System.cmd("git", [@git_config, action, @key, username])
   end
 
-  defp command(action, _usernames) do
+  defp command(_action, _usernames) do
     {:error, "Unsuported multiple users"}
   end
 end
