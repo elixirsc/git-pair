0e9194e168d0aa96eadc3c770a7c6eeabfab4447
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2020-08-13 21:59:54 -0300

    Prepare package to Hex.pm
    Co-authored-by: amadeu01 <amadeu01@users.noreply.github.com>
    Co-authored-by: leandroh <leandroh@users.noreply.github.com>
    Co-authored-by: lcidral <lcidral@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -4,10 +4,19 @@ defmodule GitPair.MixFile do
   def project do
     [
       app: :"git-pair",
+      description: "Automatically adds Co-authored-by mark to commits when you're pairing",
       version: "0.1.0",
       elixir: "~> 1.10",
       escript: escript(),
-      deps: deps()
+      deps: deps(),
+      package: package(),
+
+      # Docs
+      name: "GitPair",
+      source_url: "https://github.com/elixirsc/git-pair",
+      homepage_url: "https://hex.pm/packages/git-pair/",
+
+      docs: docs()
     ]
   end
 
@@ -19,12 +28,28 @@ defmodule GitPair.MixFile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mox, "~> 0.5.2", only: :test}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:mox, "~> 0.5.2", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
   def escript do
     [main_module: GitPair.CLI]
+  end
+
+  defp package do
+    [
+      name: "git_pair",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/elixirsc/git-pair"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "GitPair",
+      extras: ~w(README.md)
+    ]
   end
 end
