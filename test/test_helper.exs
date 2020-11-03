82442236c5fff0aba9c6afa901296bd5d4080dfa
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2020-11-02 22:15:24 -0300

    Use Hook module in Actions._modify_commit_msg/1

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,5 @@
 Mox.defmock(GitPair.SystemMock, for: GitPair.SystemBehaviour)
 Mox.defmock(GitPair.StorageMock, for: GitPair.StorageBehaviour)
+Mox.defmock(GitPair.HookMock, for: GitPair.HookBehaviour)
 
 ExUnit.start()
